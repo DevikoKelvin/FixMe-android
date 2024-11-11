82539e48801eda976c6b6dbb9519ce1a3a9e9b39
Deviko Kelvin <deviko.evangelion@gmail.com>
2024-11-11 15:24:09 +0700

    MainActivity Code Cleanup

--- app/src/main/java/com/erela/fixme/activities/MainActivity.kt
+++ app/src/main/java/com/erela/fixme/activities/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.erela.fixme.activities
 
-import android.animation.Animator
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.erela.fixme.R
 import com.erela.fixme.databinding.ActivityMainBinding
 import com.erela.fixme.helpers.UserDataHelper
 import com.erela.fixme.objects.UserData
