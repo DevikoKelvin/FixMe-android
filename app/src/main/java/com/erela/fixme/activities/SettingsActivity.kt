752fce78622647841eb41990df9dfdb0cefce502
Deviko Kelvin <deviko.evangelion@gmail.com>
2024-12-17 15:25:50 +0700

    Minor Fix on Settings

--- app/src/main/java/com/erela/fixme/activities/SettingsActivity.kt
+++ app/src/main/java/com/erela/fixme/activities/SettingsActivity.kt
@@ -1,12 +1,14 @@
 package com.erela.fixme.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.erela.fixme.BuildConfig
@@ -24,6 +26,7 @@ class SettingsActivity : AppCompatActivity() {
         ActivitySettingsBinding.inflate(layoutInflater)
     }
     private lateinit var currentAppVersion: String
+    private var downloadLink: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +41,7 @@ class SettingsActivity : AppCompatActivity() {
 
         init()
     }
+
     @SuppressLint("SetTextI18n")
     private fun init() {
         binding.apply {
@@ -45,46 +49,94 @@ class SettingsActivity : AppCompatActivity() {
                 onBackPressedDispatcher.onBackPressed()
             }
 
-            currentAppVersion = "${BuildConfig.VERSION_CODE}_${BuildConfig.VERSION_NAME}"
+            currentAppVersion = BuildConfig.VERSION_NAME
             currentAppVersionText.text = "Current app version: $currentAppVersion"
 
             checkDownloadInstallButton.setOnClickListener {
-                val appUpdaterUtils = AppUpdaterUtils(this@SettingsActivity).also {
-                    with(it) {
-                        setUpdateFrom(UpdateFrom.GITHUB)
-                        setGitHubUserAndRepo("DevikoKelvin", "FixMe-android")
-                        withListener(object : AppUpdaterUtils.UpdateListener {
-                            override fun onSuccess(update: Update?, isUpdateAvailable: Boolean?) {
-                                if (isUpdateAvailable == true) {
-                                    Log.e("Update Available", isUpdateAvailable.toString())
-                                    checkDownloadInstallText.text =
-                                        getString(R.string.download_update)
-                                    updateAvailableStatus.visibility = View.VISIBLE
-                                    currentAppVersionText.setTextColor(
-                                        ContextCompat.getColor(
-                                            this@SettingsActivity,
-                                            R.color.custom_toast_font_failed
+                /*if (checkDownloadInstallText.text == getString(R.string.download_update)) {
+                    val appUpdate = AppUpdater(this@SettingsActivity).also {
+                        with(it) {
+                            setUpdateFrom(UpdateFrom.GITHUB)
+                            setGitHubUserAndRepo("DevikoKelvin", "FixMe-android")
+                            showAppUpdated(true)
+                        }
+                    }
+
+                    appUpdate.start()
+                }*/if (downloadLink != null) {
+                    startActivity(
+                        Intent(
+                            Intent.ACTION_VIEW, downloadLink!!.toUri()
+                        )
+                    )
+                } else {
+                    val appUpdaterUtils = AppUpdaterUtils(this@SettingsActivity).also {
+                        with(it) {
+                            setUpdateFrom(UpdateFrom.GITHUB)
+                            setGitHubUserAndRepo("DevikoKelvin", "FixMe-android")
+                            withListener(object : AppUpdaterUtils.UpdateListener {
+                                override fun onSuccess(
+                                    update: Update?, isUpdateAvailable: Boolean?
+                                ) {
+                                    if (isUpdateAvailable == true) {
+                                        Log.e("Update Available", isUpdateAvailable.toString())
+                                        checkDownloadInstallText.text =
+                                            getString(R.string.download_update)
+                                        updateAvailableStatus.visibility = View.VISIBLE
+                                        currentAppVersionText.setTextColor(
+                                            ContextCompat.getColor(
+                                                this@SettingsActivity,
+                                                R.color.custom_toast_font_failed
+                                            )
                                         )
-                                    )
-                                    newAppVersionText.text =
-                                        "Detected new app version: ${update?.latestVersion}"
-                                    newAppVersionText.visibility = View.VISIBLE
-                                } else {
+                                        newAppVersionText.text =
+                                            "Detected new app version: ${update?.latestVersion}"
+                                        newAppVersionText.visibility = View.VISIBLE
+                                        downloadLink = update?.urlToDownload.toString()
+                                    } else {
+                                        CustomToast.getInstance(applicationContext)
+                                            .setMessage("No update available. Your app is on the latest version!")
+                                            .setFontColor(
+                                                ContextCompat.getColor(
+                                                    this@SettingsActivity,
+                                                    R.color.custom_toast_background_soft_blue
+                                                )
+                                            )
+                                            .setBackgroundColor(
+                                                ContextCompat.getColor(
+                                                    this@SettingsActivity,
+                                                    R.color.custom_toast_font_blue
+                                                )
+                                            ).show()
+                                        Log.e("No Update", isUpdateAvailable.toString())
+                                        currentAppVersionText.setTextColor(
+                                            ContextCompat.getColor(
+                                                this@SettingsActivity,
+                                                R.color.black
+                                            )
+                                        )
+                                        checkDownloadInstallText.text =
+                                            getString(R.string.check_for_update_now)
+                                        updateAvailableStatus.visibility = View.GONE
+                                        newAppVersionText.visibility = View.GONE
+                                    }
+                                }
+
+                                override fun onFailed(error: AppUpdaterError?) {
                                     CustomToast.getInstance(applicationContext)
-                                        .setMessage("No update available. Your app is on the latest version!")
+                                        .setMessage("Something went wrong, please try again.")
                                         .setFontColor(
                                             ContextCompat.getColor(
                                                 this@SettingsActivity,
-                                                R.color.custom_toast_background_soft_blue
+                                                R.color.custom_toast_font_failed
                                             )
                                         )
                                         .setBackgroundColor(
                                             ContextCompat.getColor(
                                                 this@SettingsActivity,
-                                                R.color.custom_toast_font_blue
+                                                R.color.custom_toast_background_failed
                                             )
                                         ).show()
-                                    Log.e("No Update", isUpdateAvailable.toString())
                                     currentAppVersionText.setTextColor(
                                         ContextCompat.getColor(
                                             this@SettingsActivity,
@@ -95,40 +147,13 @@ class SettingsActivity : AppCompatActivity() {
                                         getString(R.string.check_for_update_now)
                                     updateAvailableStatus.visibility = View.GONE
                                     newAppVersionText.visibility = View.GONE
+                                    Log.e("ERROR Update", error.toString())
                                 }
-                            }
-
-                            override fun onFailed(error: AppUpdaterError?) {
-                                CustomToast.getInstance(applicationContext)
-                                    .setMessage("Something went wrong, please try again.")
-                                    .setFontColor(
-                                        ContextCompat.getColor(
-                                            this@SettingsActivity,
-                                            R.color.custom_toast_font_failed
-                                        )
-                                    )
-                                    .setBackgroundColor(
-                                        ContextCompat.getColor(
-                                            this@SettingsActivity,
-                                            R.color.custom_toast_background_failed
-                                        )
-                                    ).show()
-                                currentAppVersionText.setTextColor(
-                                    ContextCompat.getColor(
-                                        this@SettingsActivity,
-                                        R.color.black
-                                    )
-                                )
-                                checkDownloadInstallText.text =
-                                    getString(R.string.check_for_update_now)
-                                updateAvailableStatus.visibility = View.GONE
-                                newAppVersionText.visibility = View.GONE
-                                Log.e("ERROR Update", error.toString())
-                            }
-                        })
+                            })
+                        }
                     }
+                    appUpdaterUtils.start()
                 }
-                appUpdaterUtils.start()
             }
         }
     }
