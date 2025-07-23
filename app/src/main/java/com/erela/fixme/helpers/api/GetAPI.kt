0b6559dcaeb16d70ec1522317fefe646725e3753
Deviko Kelvin <deviko.evangelion@gmail.com>
2025-07-23 15:06:51 +0700

    Update to v1.2.3

--- app/src/main/java/com/erela/fixme/helpers/api/GetAPI.kt
+++ app/src/main/java/com/erela/fixme/helpers/api/GetAPI.kt
@@ -29,7 +29,7 @@ interface GetAPI {
     @POST("checkLogin")
     fun login(
         @Field("usern") username: String,
-        @Field("pass") password: String
+        @Field("passw") password: String
     ): Call<LoginResponse>
 
     @FormUrlEncoded
